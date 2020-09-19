490a83def66cca3011bd8584f8335373cf0654bc
Murad <murad-arslangiraev-1994@mail.ru>
2020-09-19 15:05:36 +0300

    Updates

---
+++ precache-manifest.72bc0443fa164ceda07faf8dadf3f888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814e831802d45aab5ea134fa19d965ab",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "a8aab2a6cda75a4b871c",
+    "url": "/todo/static/css/main.cb01a90e.chunk.css"
+  },
+  {
+    "revision": "dd39127176b107293ab0",
+    "url": "/todo/static/js/2.90bd9f48.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo/static/js/2.90bd9f48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8aab2a6cda75a4b871c",
+    "url": "/todo/static/js/main.76bffdca.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
